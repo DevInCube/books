4667391d28b569be379e95eab81f0af8daf351cb
Ruslan Hadyniak <ruslan.hadyniak@gmail.com>
2018-12-07 15:45:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-document.write("<h2>Script loaded</h2>");
+const firestore = firebase.firestore();
+const settings = {/* your settings... */ timestampsInSnapshots: true};
+firestore.settings(settings);
+
+const books = firestore.collection("books");
+
+function toList(fbres) {
+ const mains = [];
+ fbres.forEach(b => mains.push(b));
+ return mains;
+}
+
+async function populateReference(b, referenceKey) {
+ const sub = await b.data()[referenceKey].get();
+ return {
+   id: b.id,
+   data: b.data(),
+   [referenceKey]: {
+     id: sub.id,
+     data: sub.data(),
+   },
+ };
+}
+
+books.get()
+   .then(toList)
+   .then(x => Promise.all(x.map(book => populateReference(book, "author"))))
+   .then(books => {
+     for (const book of books) {
+       document.write(`<br>"${book.data.title}" by <b>${book.author.data.fullname}</b>`);
+     }
+   });
