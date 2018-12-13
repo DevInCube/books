8bc6c77676f2d91204a6398d4704b4f87bf7a4ff
Ruslan Hadyniak <ruslan.hadyniak@gmail.com>
2018-12-13 11:39:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-async function getAllBooks() {
-  const csvUrl = `https://raw.githubusercontent.com/DevInCube/books/master/data/books.csv`;
-  const res = await fetch(csvUrl);
-  const csv = await res.text();
-  return $.csv.toObjects(csv);
+function makeApiCall(sheetId, query, callback) {
+  // Note: The below spreadsheet is "Public on the web" and will work
+  // with or without an OAuth token.  For a better test, replace this
+  // URL with a private spreadsheet.
+  const encodedQuery = encodeURIComponent(query);
+  const tqUrl = `https://docs.google.com/spreadsheets` +
+      `/d/${sheetId}/gviz/tq` +
+      `?tqx=responseHandler:handleTqResponse` +
+      `&tq=${encodedQuery}`;
+
+  document.write('<script src="' + tqUrl +'" type="text/javascript"></script>');
+    
+  function handleTqResponse(resp) {
+      callback(null, resp);
+  }
+}
+
+
+function parseBooks(resp) {
+    const headers = {};
+    for (const [ci, col] of resp.table.cols.entries()) {
+        headers[col.label] = ci;
+    }
+    const books = [];
+    for (const row of resp.table.rows) {
+        const book = {};
+        for (const [key, pos] of Object.entries(headers)) {
+            book[key] = row.c[pos] ? row.c[pos].v : null;
+        }
+        books.push(book)
+    }
+    return books;
 }
 
 const app = new Vue({
@@ -12,9 +39,12 @@ const app = new Vue({
     allBooks: [],
   },
   mounted() {
-      getAllBooks().then(books => {
-          this.allBooks = books;
-       console.log(books);
-      });
+      makeApiCall(`14xSwU7g8-8F88IBdO2rhoH8PJ6BTJf0yqeThlKVX9mY/gviz`, 
+            `SELECT *`, 
+            (err, resp) => {
+                const books = parseBooks(resp);
+                this.allBooks = books;
+                console.log(books);
+            });
   },
 });
