ee237505b4984bfad61e54e48096ddf7d542360d
phadyniak <public.hadyniak@gmail.com>
2018-12-21 10:11:10 +0200

    ua lang; upd links

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 function makeApiCall(sheetId, query) {
-  // Note: The below spreadsheet is "Public on the web" and will work
-  // with or without an OAuth token.  For a better test, replace this
-  // URL with a private spreadsheet.
-  const encodedQuery = encodeURIComponent(query);
-  const tqUrl = `https://docs.google.com/a/google.com` + 
-        `/spreadsheets/d/${sheetId}/gviz/tq` + 
-        `?tqx=responseHandler:handleTqResponse` + 
+    // Note: The below spreadsheet is "Public on the web" and will work
+    // with or without an OAuth token.  For a better test, replace this
+    // URL with a private spreadsheet.
+    const encodedQuery = encodeURIComponent(query);
+    const tqUrl = `https://docs.google.com/a/google.com` +
+        `/spreadsheets/d/${sheetId}/gviz/tq` +
+        `?tqx=responseHandler:handleTqResponse` +
         `&tq=${encodedQuery}`;
 
-  document.write('<script src="' + tqUrl +'" type="text/javascript"></script>');
+    document.write('<script src="' + tqUrl + '" type="text/javascript"></script>');
 }
-
 
 function parseBooks(resp) {
     const headers = {};
@@ -29,77 +28,82 @@ function parseBooks(resp) {
 }
 
 const app = new Vue({
-  el: '#app',
-  data: {
-    loading: true,
-    allBooks: [],
-    filterString: ``,
-    filters: {
-        showRented: true,
+    el: '#app',
+    data: {
+        loading: true,
+        allBooks: [],
+        filterString: ``,
+        filters: {
+            showRented: true,
+        },
+        page: 1,
+        itemsPerPage: 10,
+        statics: {
+            defaultCoverUrl: `https://vignette.wikia.nocookie.net/summoner-the-novice/images/e/e2/Placeholder_02.png/revision/latest?cb=20180210125541`,
+            goodreadsLogoUrl: `https://images-eu.ssl-images-amazon.com/images/I/61Fkb1F2vOL.png`,
+            telegramLogoUrl: `https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Telegram_alternative_logo.svg/384px-Telegram_alternative_logo.svg.png`,
+            instagramLogoUrl: `https://instagram-brand.com/wp-content/uploads/2016/11/app-icon2.png`,
+            telegramChannelLink: `https://t.me/rabooks`,
+        },
+        user: {
+            fullname: `Руслан Гадиняк`,
+            imageUrl: `images/users/rhadyniak/photo.jpg`,
+            region: `Київ`,
+            goodreadsLink: `https://www.goodreads.com/user/show/51764433-ruslan-hadyniak`,
+            telegramLink: `https://t.me/rhadyniak`,
+            instagramLink: `https://www.instagram.com/rhadyniak/`,
+        },
     },
-    page: 1,
-    itemsPerPage: 10,
-    statics: {
-        defaultCoverUrl: `https://vignette.wikia.nocookie.net/summoner-the-novice/images/e/e2/Placeholder_02.png/revision/latest?cb=20180210125541`,
-        goodreadsLogoUrl: `https://images-eu.ssl-images-amazon.com/images/I/61Fkb1F2vOL.png`,
-        telegramLogoUrl: `https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Telegram_alternative_logo.svg/384px-Telegram_alternative_logo.svg.png`,
+    computed: {
+        filteredBooks() {
+            const filteredBooks = this.allBooks
+                .filter(x =>
+                    (this.filters.showRented || !x.isRented)
+                    && (this.filterString.trim() === '' || this.filterString.split(/\s+/g)
+                        .filter(x => x)
+                        .filter(part => `${x.title} ${x.author}`.toLowerCase().includes(part.toLowerCase())).length > 0));
+            return filteredBooks;
+        },
+        pages() {
+            const filteredBooks = this.filteredBooks;
+            const pages = Math.ceil(filteredBooks.length / this.itemsPerPage);
+            return pages;
+        },
+        pageBooks() {
+            const filteredBooks = this.filteredBooks;
+            const pages = this.pages;
+            if (this.page > pages) this.page = pages;
+            if (this.page < 1) this.page = 1;
+            const from = (this.page - 1) * this.itemsPerPage;
+            return filteredBooks.slice(from, from + this.itemsPerPage);
+        },
+        notFound() { return !this.loading && this.filteredBooks.length === 0; }
     },
-    user: {
-        fullname: `Ruslan Hadyniak`,
-        imageUrl: `https://cdn4.telesco.pe/file/G0iSz_GIOUdLRdXC-5-0J9FI2En6EOP2yPQavVtUgq6iAkUOFSu4A-XX_wWkUw1PglWsRObhcjKNmRUMLZbDPdpvqEN-i6YUDED__qRChpyiIT_qcymGQK_wqhRENxYpf4qJAjOFOoUMto-FFac2sLbj8Cmmu6QQxV9BOwhhe2ry51w1-7ASL4WKV5ecus5DOF5dq19d8tbkeG_iANnq09vklxZhZG3Sl9xdx1DOQLDrkECuQ7PX4bwRHjcnvQraFU4rmDE1m5kWczEsHfaPbmcAPCrUbz2DKNZgjNTYG4-INcogAVivB3cFdPXY5uyiZosjQVeJAQ2NxkX5kQAVHA.jpg`,
-        region: `Kyiv`,
-        goodreadsLink: `https://www.goodreads.com/user/show/51764433-ruslan-hadyniak`,
-        telegramLink: `https://t.me/rhadyniak`,
+    mounted() {
+        makeApiCall(`14xSwU7g8-8F88IBdO2rhoH8PJ6BTJf0yqeThlKVX9mY`, `SELECT *`);
+    },
+    methods: {
+        setBooks(books) {
+            this.allBooks = books.filter(x => x.isAvailable);
+            this.loading = false;
+        },
+        toggleRented(e) {
+            this.filters.showRented = !this.filters.showRented;
+        },
+        clearFilter(e) {
+            this.filterString = ``;
+        },
+        filterByContents(e) {
+            const contents = e.target.innerText;
+            this.filterString = contents.trim();
+        },
+        prevPage() {
+            this.page -= 1;
+        },
+        nextPage() {
+            this.page += 1;
+        },
     }
-  },
-  computed: {
-    filteredBooks() {
-        const filteredBooks = this.allBooks
-            .filter(x => 
-                (this.filters.showRented || !x.isRented) 
-                && `${x.title} ${x.author}`.includes(this.filterString));
-        return filteredBooks;
-    },
-    pages() {
-        const filteredBooks = this.filteredBooks;
-        const pages = Math.ceil(filteredBooks.length / this.itemsPerPage);
-        return pages;
-    },
-    pageBooks() {
-        const filteredBooks = this.filteredBooks;
-        const pages = this.pages;
-        if (this.page > pages) this.page = pages;
-        if (this.page < 1) this.page = 1;
-        const from = (this.page - 1) * this.itemsPerPage;
-        return filteredBooks.slice(from, from + this.itemsPerPage);
-    },
-    notFound() { return !this.loading && this.filteredBooks.length === 0; }
-  },
-  mounted() {
-      makeApiCall(`14xSwU7g8-8F88IBdO2rhoH8PJ6BTJf0yqeThlKVX9mY`, `SELECT *`);
-  },
-  methods: {
-    setBooks(books) {
-        this.allBooks = books.filter(x => x.isAvailable);
-        this.loading = false;
-    },
-    toggleRented(e) {
-        this.filters.showRented = !this.filters.showRented;
-    },
-    clearFilter(e) {
-        this.filterString = ``;
-    },
-    filterByContents(e) {
-        const contents = e.target.innerText;
-        this.filterString = contents.trim();
-    },
-    prevPage() {
-        this.page -= 1;
-    },
-    nextPage() {
-        this.page += 1;
-    },
-  }
 });
 
 function handleTqResponse(resp) {
